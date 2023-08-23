8a04e9a18b9ef9bb21bb09e0d40c6e6e6e11d781
adamdross27 <104739164+adamdross27@users.noreply.github.com>
2023-08-23 01:55:25 +0000

    Made drop down menu which has appointment

---
+++ src/Appointment.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import './CSS files/Appointment.css';
+    
+    function Appointment() 
+    {
+        const[isOpen, setIsOpen] = useState(false);   
+        const[selectedItem, setSelectedItem] = useState(null);
+    
+        const toggleDropdown = () =>
+        {
+            setIsOpen(!isOpen);
+        };
+
+        const handleItemClick = (item) =>
+        {
+            setSelectedItem(item);
+        };
+
+        const items = [
+            {id: 1, name: 'Appointment'},
+        ]
+
+    return (
+      <div className="appointment">
+          <button onClick={toggleDropdown} className="appointment-toggle"> 
+            Add Event
+           </button>   
+           {isOpen && (
+            <ul className="appointment-menu">
+                {items.map((item) => (
+                    <li
+                    key={item.id}
+                    className={selectedItem === item ? 'selected' : ''}
+                    onClick={() => handleItemClick(item)}
+                    >
+                       {item.name} 
+                    </li>
+                ))}
+            </ul>
+           )} 
+      </div>
+
+    );
+}
+
+export default Appointment
