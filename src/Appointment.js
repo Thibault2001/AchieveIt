4c648d2e8844e76df686e79d6d4ba660b44d3c8e
adamdross27 <104739164+adamdross27@users.noreply.github.com>
2023-08-28 03:06:51 +0000

    Added modal and completed dropdown menu

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,21 +1,38 @@
 import React, { useState } from 'react'
 import './CSS files/Appointment.css';
+import Modal from 'react-modal';
     
     function Appointment() 
     {
-        const[isOpen, setIsOpen] = useState(false);   
+        const[isModalOpen, setIsModalOpen] = useState(false);
         const[selectedItem, setSelectedItem] = useState(null);
+        const[isDropdownOpen, setIsDropdownOpen] = useState(false);
     
-        const toggleDropdown = () =>
-        {
-            setIsOpen(!isOpen);
-        };
-
         const handleItemClick = (item) =>
         {
             setSelectedItem(item);
+
+            if(item.name === 'Appointment')
+            {
+                handleAppointmentClick();
+            }
         };
 
+        const handleAppointmentClick = () =>
+        {
+                setIsModalOpen(true);
+        };
+
+        const closeModal = () =>
+        {
+            setIsModalOpen(false);
+        };
+
+        const toggleDropdown = () =>
+        {
+            setIsDropdownOpen(!isDropdownOpen);
+        };
+    
         const items = [
             {id: 1, name: 'Appointment'},
             {id: 2, name: 'Sports'}
@@ -26,7 +43,9 @@ import './CSS files/Appointment.css';
           <button onClick={toggleDropdown} className="appointment-toggle"> 
             Add Event
            </button>   
-           {isOpen && (
+           
+            
+            {isDropdownOpen && (
             <ul className="appointment-menu">
                 {items.map((item) => (
                     <li
@@ -39,7 +58,20 @@ import './CSS files/Appointment.css';
                     
                 ))}
             </ul>
-           )} 
+            )}
+           <Modal
+           isOpen={isModalOpen}
+           onRequestClose={closeModal}
+           contentLabel="Popup Modal"
+           >
+            <h1> Add Appointment Event </h1>
+            <p> Appointment Title</p>
+            <p> Appointment Location </p>
+            <p> Appointment Date </p>
+            <p> Appointment Time </p>
+            <button onClick={closeModal}> Close </button>
+
+           </Modal>
       </div>
 
     );
