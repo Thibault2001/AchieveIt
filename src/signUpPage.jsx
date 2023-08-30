453e64b12b45a20599812010fc63ac677c39dcc9
Thibault2001 <thibaultsera@gmail.com>
2023-08-31 11:42:22 +1200

    Add comments

--- src/signUpPage.jsx
+++ src/signUpPage.jsx
@@ -1,22 +1,28 @@
+// This component defines a sign-up page that allows users to create new accounts.
+// It provides a form for entering email and password, handles form submission,
+// and displays error messages if the sign-up process fails.
 
+// Import necessary libraries and functions
 import { useState } from 'react';
-import { createUserWithEmailAndPassword } from 'firebase/auth';  
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from './firebase';
 
-
+// Define the SignUpPage component
 function SignUpPage() {
-
+  // State variables for form input, password, and error handling
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
 
+  // Function to handle form submission
   const handleSubmit = async (event) => {
     event.preventDefault();
-    
+
     try {
+      // Attempt to create a new user with email and password
       await createUserWithEmailAndPassword(auth, email, password);
-      
     } catch (err) {
+      // Handle specific error codes and display appropriate error messages
       if (err.code === 'auth/email-already-in-use') {
         setError('This email is already used');
       } else if (err.code === 'auth/weak-password') {
@@ -24,32 +30,35 @@ function SignUpPage() {
       } else {
         setError('Error signing up');
       }
-    }    
-
+    }
   };
 
+  // Render the SignUpPage component
   return (
     <div>
+      {/* Display error message if an error exists */}
       {error && <p>{error}</p>}
-    <form onSubmit={handleSubmit}>
-      <input 
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)} 
-      />
+      {/* Form for signing up */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-      <input
-        type="password" 
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}  
-      />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-      <button type="submit">Sign Up</button>
-    </form>
+        <button type="submit">Sign Up</button>
+      </form>
     </div>
   );
 }
 
+// Export the SignUpPage component
 export default SignUpPage;
