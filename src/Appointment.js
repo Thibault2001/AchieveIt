bb918f5cbb335d5b267e482e99d34331a87a5387
Kaylz226 <105680897+Kaylz226@users.noreply.github.com>
2023-09-13 23:55:54 +0000

    MAking new branch

--- src/Appointment.js
+++ src/Appointment.js
@@ -37,7 +37,8 @@ import EventDisplay from './EventDisplay'
             {id: 1, name: 'Appointment'},
             {id: 2, name: 'Sports'},
             {id: 3, name: 'Birthday'},
-            {id: 4, name: 'University'}
+            {id: 4, name: 'University'},
+            {id:5, name: 'Goals'}
         ]
 
     return (
