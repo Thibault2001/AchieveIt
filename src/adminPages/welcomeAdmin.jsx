8096015052c03f2961e8fe9e1ed9fe9ab61a397e
Thibault2001 <thibaultsera@gmail.com>
2023-09-21 11:50:26 +1200

    comments and fix

--- src/adminPages/welcomeAdmin.jsx
+++ src/adminPages/welcomeAdmin.jsx
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+
+// Admin Page Component
+// This component represents the admin page. It allows an administrator to execute a script
+// by providing an email address. The user is considered an administrator if they have a
+// "admin" cookie. If the user is not an admin, they will see an "Access Denied" message.
 
 const AdminPage = () => {
+  // State variables
   const [email, setEmail] = useState('');
-  const [isAdmin, setIsAdmin] = useState(false); // Add state to check if the user is an admin
-  const navigate = useNavigate();
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [ setAccessDenied] = useState(false); // State to track access denied
 
-  // Effect to check if the user is an admin when the page loads
+  // Check if the user has an "admin" cookie when the page loads
   useEffect(() => {
     const cookieValue = document.cookie
       .split('; ')
@@ -18,6 +23,7 @@ const AdminPage = () => {
     }
   }, []);
 
+  // Function to handle script execution
   const handleExecuteScript = async () => {
     try {
       const response = await fetch('http://localhost:3001/execute-script', {
@@ -35,25 +41,72 @@ const AdminPage = () => {
     }
   };
 
-  // If the user is not an admin, redirect them to the login page
-  if (!isAdmin) {
-    navigate('/login');
-    return null;
-  }
+  // Check if the user is not an admin and set the accessDenied state
+  useEffect(() => {
+    if (!isAdmin) {
+      setAccessDenied(true);
+    }
+  }, [isAdmin]);
+
+  // Function to close the access denied message
+  const handleCloseAccessDenied = () => {
+    setAccessDenied(false);
+  };
 
   return (
     <div>
-      <h1>Admin Page</h1>
-      <p>Add admin role to user:</p>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <button onClick={handleExecuteScript}>Execute Script</button>
+      {isAdmin ? (
+        // If the user is an admin, display the admin page content
+        <>
+          <h1>Admin Page</h1>
+          <p>Add admin role to user:</p>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button onClick={handleExecuteScript}>Execute Script</button>
+        </>
+      ) : (
+        // If the user is not an admin, display the "Access Denied" message
+        <div style={styles.accessDenied}>
+          <input
+            type="text"
+            value="Access Denied. You are not authorized to view this page."
+            readOnly
+            style={styles.accessDeniedInput}
+          />
+        </div>
+      )}
     </div>
   );
+};
+
+// Styles for the "Access Denied" message and close button
+const styles = {
+  accessDenied: {
+    backgroundColor: '#f2f2f2',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    padding: '10px',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  accessDeniedInput: {
+    flex: '1',
+    border: 'none',
+    backgroundColor: 'transparent',
+    color: 'red',
+    fontSize: '16px',
+  },
+  closeButton: {
+    backgroundColor: '#ccc',
+    border: 'none',
+    borderRadius: '5px',
+    marginLeft: '10px',
+    cursor: 'pointer',
+  },
 };
 
 export default AdminPage;
