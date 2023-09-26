d61c56d2cd6237bbd1b349f868f964d0358074e9
Thibault2001 <thibaultsera@gmail.com>
2023-09-26 21:15:03 +1300

    pop up and set time for redirect

--- src/signUpPage.jsx
+++ src/signUpPage.jsx
@@ -5,16 +5,23 @@
 // Import necessary libraries and functions
 import './CSS_files/App.css';
 import { useState } from 'react';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword,signOut } from 'firebase/auth';
 import { auth } from './firebase';
 import { getDatabase, ref, set } from 'firebase/database';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from 'react-router-dom';
 
 // Define the SignUpPage component
 function SignUpPage() {
-  // State variables for form input, password, and error handling
+  // State variables for form input, password, name, and error handling
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
   const [error, setError] = useState(null);
+
+  // Get the navigate function
+  const navigate = useNavigate();
 
   // Function to handle form submission
   const handleSubmit = async (event) => {
@@ -26,10 +33,24 @@ function SignUpPage() {
       // Get user ID and email
       const userId = userCredential.user.uid;
 
-      // Store ID in the real time Database
+      // Sign out the user immediately after registration
+      await signOut(auth);
+
+      // Store the user's name in the real-time Database along with other information
       const db = getDatabase();
       const usersRef = ref(db, 'users/' + userId);
-      set(usersRef, email);
+      set(usersRef, {
+        email: email,
+        name: name,
+      });
+
+      // Display a success notification
+      toast.success('Registration successful!');
+
+      // Redirect the user to the /login page after successful registration and 5s
+      setTimeout(() => {
+        navigate('/login');
+      }, 5000);
     } catch (err) {
       // Handle specific error codes and display appropriate error messages
       if (err.code === 'auth/email-already-in-use') {
@@ -50,6 +71,13 @@ function SignUpPage() {
       {/* Form for signing up */}
       <form onSubmit={handleSubmit}>
         <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
           type="email"
           placeholder="Email"
           value={email}
@@ -65,6 +93,9 @@ function SignUpPage() {
 
         <button type="submit">Sign Up</button>
       </form>
+
+      {/* ToastContainer to display notifications */}
+      <ToastContainer autoClose={5000} />
     </div>
   );
 }
