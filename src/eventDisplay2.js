3f2847254e2b9dbf6c8d54c483c8414d4ba17d51
Thibault2001 <thibaultsera@gmail.com>
2023-10-03 12:26:14 +1300

    add delete feature for event display

--- src/eventDisplay2.js
+++ src/eventDisplay2.js
@@ -1,13 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { GetColour } from "./Event";
-import { Box, Typography, Card } from "@mui/material"; // Import Card from MUI
-import { db, ref, onValue } from "./firebase";
-import { auth } from "./firebase";
-import "./CSS_files/EventDisplay.css";
+import { Box, Typography, Card } from "@mui/material";
+import { auth, db, ref, onValue, set } from "./firebase";
 
-// Define reminder time options
 const reminderTimeOptions = {
-  //Defining options for the reminder time
   at_event: 'At Time of Event',
   '5': '5 Minutes',
   '10': '10 Minutes',
@@ -26,6 +22,7 @@ const reminderTimeOptions = {
 
 const EventDisplay = () => {
   const [events, setEvents] = useState([]);
+  const [selectedEvents, setSelectedEvents] = useState([]);
   const [, setUserID] = useState(null);
 
   useEffect(() => {
@@ -49,48 +46,76 @@ const EventDisplay = () => {
     }
   }, []);
 
+  const handleEventSelect = (event) => {
+    if (selectedEvents.includes(event.eventID)) {
+      setSelectedEvents(selectedEvents.filter((id) => id !== event.eventID));
+    } else {
+      setSelectedEvents([...selectedEvents, event.eventID]);
+    }
+  };
+
+  const handleDeleteSelectedEvents = () => {
+    selectedEvents.forEach((eventID) => {
+      const user = auth.currentUser;
+      const userID = user.uid;
+      setUserID(userID);
+      const eventRef = ref(db, `calendar/${userID}/events/${eventID}`);
+      set(eventRef, null);
+    });
+
+    setSelectedEvents([]);
+  };
+
   return (
     <Box>
       <Typography variant="h4">Events</Typography>
-      <div className="events-container">
+      <button onClick={handleDeleteSelectedEvents}>Delete Selected Events</button>
+      <div>
         {events.length > 0 ? (
-          events.map((event) => (
-            <Card
-              className="event-card"
-              style={{ backgroundColor: GetColour(event.eventType) }}
-              key={event.eventID}
-            >
-              <h2>
-                <u>{event.eventTitle}</u>
-              </h2>
-              <br />
-              <h3>
-                Event Type: <u>{event.eventType}</u>
-              </h3>{" "}
-              <br />
-              <h3>
-                Date: <u>{event.eventDate}</u>
-              </h3>
-              <br />
-              <h3>
-                Time: <u>{event.eventTime}</u>
-              </h3>
-              <br />
-              <h3>
-                Reminder will be sent{" "}
-                <u>
-                  {event.reminderTime === "at_event"
-                    ? `${reminderTimeOptions[event.reminderTime]}.`
-                    : `${reminderTimeOptions[event.reminderTime]} before event.`}
-                </u>
-              </h3>
-              <p>
-                <b>Details:</b>
+          <div className="event-display-container">
+            {events.map((event) => (
+              <Card
+                className={`event-display-card ${selectedEvents.includes(event.eventID) ? 'selected' : ''}`}
+                style={{ backgroundColor: GetColour(event.eventType) }}
+                key={event.eventID}
+              >
+                <input
+                  type="checkbox"
+                  checked={selectedEvents.includes(event.eventID)}
+                  onChange={() => handleEventSelect(event)}
+                />
+                <h2>
+                  <u>{event.eventTitle}</u>
+                </h2>
                 <br />
-                {event.eventDescription}
-              </p>
-            </Card>
-          ))
+                <h3>
+                  Event Type: <u>{event.eventType}</u>
+                </h3>{" "}
+                <br />
+                <h3>
+                  Date: <u>{event.eventDate}</u>
+                </h3>
+                <br />
+                <h3>
+                  Time: <u>{event.eventTime}</u>
+                </h3>
+                <br />
+                <h3>
+                  Reminder will be sent{" "}
+                  <u>
+                    {event.reminderTime === "at_event"
+                      ? `${reminderTimeOptions[event.reminderTime]}.`
+                      : `${reminderTimeOptions[event.reminderTime]} before event.`}
+                  </u>
+                </h3>
+                <p>
+                  <b>Details:</b>
+                  <br />
+                  {event.eventDescription}
+                </p>
+              </Card>
+            ))}
+          </div>
         ) : (
           <p>No events to display.</p>
         )}
