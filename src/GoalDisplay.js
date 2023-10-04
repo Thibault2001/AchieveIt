9a990fd7a40e4bc78032d0983f8c3ccc4146bdc3
Kaylz226 <105680897+Kaylz226@users.noreply.github.com>
2023-10-04 22:16:33 +0000

    Adding subgoals

--- src/GoalDisplay.js
+++ src/GoalDisplay.js
@@ -3,42 +3,41 @@ import { Event } from './Event.js';
 import './CSS_files/App.css';
 import './CSS_files/Event.css';
 
-/*
-  This function creates all the details and fields needed for the goal details pop
-  up modal. 
-*/
 function GoalDisplay({ selectedItem, closeModal }) {
-  //create all the state variables
   const [events, setEvents] = useState([]);
   const [title, setTitle] = useState(selectedItem ? selectedItem.name : '');
   const [type, setType] = useState('');
   const [date, setDate] = useState('');
   const [descrip, setDescrip] = useState('');
+  const [showSubgoalsCheckbox, setShowSubgoalsCheckbox] = useState(false); // Initially hidden
+  const [showSubgoals, setShowSubgoals] = useState(false); // To control subgoals visibility
+  const [subgoalTitle, setSubgoalTitle] = useState('');
+  const [subgoalDate, setSubgoalDate] = useState('');
+  const [subgoalTime, setSubgoalTime] = useState('');
+  const [subGoalDesc, setSubGoalDesc] = useState('');
 
-  //Allows a user to enter a Title for the goal
   const titleChange = (event) => {
     setTitle(event.target.value);
   };
 
-  //Allows the user to set the due date for the goal
   const dateChange = (event) => {
+    const selectedDate = new Date(event.target.value);
+    const currentDate = new Date();
+    const threeMonthsFromNow = new Date(currentDate);
+    threeMonthsFromNow.setMonth(currentDate.getMonth() + 3);
+
     setDate(event.target.value);
+    setShowSubgoalsCheckbox(selectedDate > threeMonthsFromNow);
   };
 
-  //allows the user to add a description for the goal
   const descChange = (event) => {
     setDescrip(event.target.value);
   };
 
-
   const handleCreateEvent = () => {
-    // Get the current date as a JavaScript Date object
     const currentDate = new Date();
-
-    // Parse the selected date as a JavaScript Date object
     const selectedDate = new Date(date);
 
-    // Check if the selected date is in the future
     if (selectedDate > currentDate) {
       const newEvent = {
         id: events.length + 1,
@@ -48,59 +47,123 @@ function GoalDisplay({ selectedItem, closeModal }) {
         description: descrip,
       };
 
-      //clear the fields for the user to add another goal
       setEvents([...events, newEvent]);
       setTitle('');
       setType('');
       setDate('');
       setDescrip('');
+      setShowSubgoals(false); // Hide subgoals after creating the main goal
     } else {
-      // Display an error message or handle the case when the date is not in the future
       alert('Please select a date in the future.');
     }
   };
 
-  //display the pop up modal for the user to see and fill in their
-  //goal details
+  const handleCreateSubgoal = () => {
+    const newSubgoal = {
+      id: events.length + 1, // Use the same ID counter for subgoals
+      title: subgoalTitle,
+      date: subgoalDate,
+      desc: subGoalDesc,
+      time: subgoalTime,
+    };
+
+    setEvents([...events, newSubgoal]); // Add subgoal to events
+    setSubgoalTitle('');
+    setSubgoalDate('');
+    setSubgoalTime('');
+    setSubGoalDesc('');
+  };
+
   return (
-    <div className="createEvent">
-      <p>{selectedItem ? selectedItem.name : ''} Title:</p>
-      <input
-        type="text"
-        onChange={titleChange}
-        placeholder="Enter Title"
-      />
+    <div className="createGoal">
+  <div className="left-align">
+    <p>{selectedItem ? selectedItem.name : ''} Title:</p>
+    <textarea
+      id="textAreaTitle"
+      rows='2'
+      cols = '50'
+      onChange={titleChange}
+      placeholder="Enter Title"
+      className="enter-title"
+    ></textarea>
+  </div>
 
+  <p>Date:</p>
+  <input
+    type="date"
+    value={date}
+    onChange={dateChange}
+  />
+
+  <p>{selectedItem ? selectedItem.name : ''} Description:</p>
+  <textarea
+    id="textAreaDescription"
+    rows="5"
+    cols="50"
+    placeholder="Enter your description here..."
+    onChange={setSubGoalDesc}
+  ></textarea>
+<p> </p>
+<button onClick={handleCreateEvent}>Create Goal</button>
+
+  {showSubgoalsCheckbox && (
+    <label>
+      <input
+        type="checkbox"
+        onChange={() => setShowSubgoals(!showSubgoals)}
+        checked={showSubgoals}
+      />
+      Show Subgoals
+    </label>
+  )}
+
+  {showSubgoals && (
+    <div>
+      <h3>Create Subgoals</h3>
+      <p>Title: </p>
+       <textarea
+          id="textAreaTitle"
+          rows='2'
+          cols = '50'
+          placeholder="Subgoal Title"
+          value={subgoalTitle}
+          onChange={(e) => setSubgoalTitle(e.target.value)}
+          className='enter-title'
+        />
       <p>Date:</p>
-      <input
-        type="date"
-        value={date}
-        onChange={dateChange}
-      />
+  <input
+    type="date"
+    value={date}
+    onChange={setSubgoalDate}
+  />
 
-      <p>{selectedItem ? selectedItem.name : ''} Description:</p>
-      <textarea
-        id="textAreaDescription"
-        rows="5"
-        cols="50"
-        placeholder="Enter your description here..."
-        onChange={descChange}
-      ></textarea>
-
-      <div className="eventHolder">
-        {events.map((event) => (
-          <Event
-            key={event.id}
-            title={event.title}
-            type={selectedItem ? selectedItem.name : ''}
-            date={event.date}
-            time={event.time}
-            description={event.description}
-          />
-        ))}
-      </div>
-      <button onClick={handleCreateEvent}>Create Goal</button>
+  <p>{selectedItem ? selectedItem.name : ''} Description:</p>
+  <textarea
+    id="textAreaDescription"
+    rows="5"
+    cols="50"
+    placeholder="Enter your description here..."
+    onChange={descChange}
+  ></textarea>
+      <p></p>
+      <button onClick={handleCreateSubgoal}>Create Subgoal</button>
     </div>
+  )}
+
+  <div className="eventHolder">
+    {events.map((event) => (
+      <Event
+        key={event.id}
+        title={event.title}
+        type={selectedItem ? selectedItem.name : ''}
+        date={event.date}
+        time={event.time}
+        description={event.description}
+      />
+    ))}
+</div>
+</div>
+
   );
 }
 
