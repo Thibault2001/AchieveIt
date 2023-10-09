668e56c5b5c0dad2535b38896483f938e10a6453
Kaylz226 <105680897+Kaylz226@users.noreply.github.com>
2023-10-09 01:26:47 +0000

    Adding Subgoals again as previous work disappeared

--- src/GoalDisplay.js
+++ src/GoalDisplay.js
@@ -2,15 +2,21 @@ import React, { useState } from 'react';
 import './CSS_files/App.css';
 import './CSS_files/Event.css';
 import { Event } from './Event.js';
-import { auth, ref, set, db } from './firebase'; // Assurez-vous d'importer Firebase correctement.
+import { auth, ref, set, db } from './firebase';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-function GoalDisplay({ closeModal }) {
-  const [goals] = useState([]); // Utilisez "goals" pour stocker les objectifs
+function GoalDisplay({ selectedItem, closeModal }) {
+  const [goals, setGoals] = useState([]);
   const [title, setTitle] = useState('');
   const [date, setDate] = useState('');
   const [descrip, setDescrip] = useState('');
+  const [showSubgoalsCheckbox, setShowSubgoalsCheckbox] = useState(false);
+  const [showSubgoals, setShowSubgoals] = useState(false);
+  const [subgoalTitle, setSubgoalTitle] = useState('');
+  const [subgoalDate, setSubgoalDate] = useState('');
+  const [subgoalTime, setSubgoalTime] = useState('');
+  const [subGoalDesc, setSubGoalDesc] = useState('');
 
   const user = auth.currentUser;
   const userID = user ? user.uid : '';
@@ -20,7 +26,12 @@ function GoalDisplay({ closeModal }) {
   };
 
   const dateChange = (event) => {
+    const selectedDate = new Date(event.target.value);
+    const currentDate = new Date();
+    const threeMonthsFromNow = new Date(currentDate);
+    threeMonthsFromNow.setMonth(currentDate.getMonth() + 3);
     setDate(event.target.value);
+    setShowSubgoalsCheckbox(selectedDate > threeMonthsFromNow);
   };
 
   const descChange = (event) => {
@@ -28,38 +39,62 @@ function GoalDisplay({ closeModal }) {
   };
 
   const handleCreateGoal = () => {
-    const newGoal = {
-      title: title,
-      date: date,
-      description: descrip,
+    const currentDate = new Date();
+    const selectedDate = new Date(date);
+    if (selectedDate > currentDate) {
+      const newGoal = {
+        id: goals.length + 1,
+        description: descrip,
+      };
+
+      const goalRef = ref(db, `calendar/${userID}/goals/${title}`);
+
+      set(goalRef, newGoal)
+        .then(() => {
+          toast.success('Goal Created Successfully!');
+          setGoals([...goals, newGoal]);
+          setTitle('');
+          setDate('');
+          setDescrip('');
+          setShowSubgoals(false);
+        })
+        .catch((error) => {
+          toast.error('Failed to Create Goal.');
+        });
+    } else {
+      alert('Please select a date in the future.');
+    }
+  };
+
+  const handleCreateSubgoal = () => {
+    const newSubgoal = {
+      id: goals.length + 1,
+      title: subgoalTitle,
+      date: subgoalDate,
+      desc: subGoalDesc,
+      time: subgoalTime,
     };
-
-    const goalRef = ref(db, `calendar/${userID}/goals/${title}`); // Utilisez l'ID de l'utilisateur pour stocker les objectifs dans la base de données
-
-    set(goalRef, newGoal)
-      .then(() => {
-        toast.success('Goal Created Successfully!');
-        // Vous pouvez également mettre à jour l'état local ici si nécessaire.
-      })
-      .catch((error) => {
-        toast.error('Failed to Create Goal.');
-      });
-
-    // Réinitialisez les champs du formulaire.
-    setTitle('');
-    setDate('');
-    setDescrip('');
+    setGoals([...goals, newSubgoal]);
+    setSubgoalTitle('');
+    setSubgoalDate('');
+    setSubgoalTime('');
+    setSubGoalDesc('');
   };
 
   return (
-    <div className="createEvent">
-      <p>Title:</p>
-      <input
-        type="text"
-        onChange={titleChange}
-        value={title}
-        placeholder="Enter Title"
-      />
+    <div className="createGoal">
+      <div className="left-align">
+        <p>Title:</p>
+        <textarea
+          id="textAreaTitle"
+          rows="2"
+          cols="50"
+          onChange={titleChange}
+          value={title}
+          placeholder="Enter Title"
+          className="enter-title"
+        ></textarea>
+      </div>
 
       <p>Date:</p>
       <input
@@ -73,23 +108,68 @@ function GoalDisplay({ closeModal }) {
         id="textAreaDescription"
         rows="5"
         cols="50"
-        value={descrip}
-        onChange={descChange}
         placeholder="Enter your description here..."
+        onChange={descChange}
       ></textarea>
+      <p></p>
+      <button onClick={handleCreateGoal}>Create Goal</button>
 
+      {showSubgoalsCheckbox && (
+        <label>
+          <input
+            type="checkbox"
+            onChange={() => setShowSubgoals(!showSubgoals)}
+            checked={showSubgoals}
+          />
+          Show subgoals
+        </label>
+      )}
+
+      {showSubgoals && (
+        <div>
+          <h3>Create Subgoals</h3>
+          <p>Title:</p>
+          <textarea
+            id="textAreaTitle"
+            rows="2"
+            cols="50"
+            placeholder="Subgoal Title"
+            value={subgoalTitle}
+            onChange={(e) => setSubgoalTitle(e.target.value)}
+            className="enter-title"
+          />
+
+          <p>Date:</p>
+          <input
+            type="date"
+            value={subgoalDate}
+            onChange={(e) => setSubgoalDate(e.target.value)}
+          />
+
+          <p>Description:</p>
+          <textarea
+            id="textAreaDescription"
+            rows="5"
+            cols="50"
+            placeholder="Enter your description here..."
+            onChange={descChange}
+          ></textarea>
+          <p></p>
+          <button onClick={handleCreateSubgoal}>Create Subgoal</button>
+        </div>
+      )}
       <div className="eventHolder">
-        {goals.map((goal, index) => (
+        {goals.map((goal) => (
           <Event
-            key={index}
+            key={goal.id}
             title={goal.title}
+            type={selectedItem ? selectedItem.name : ''}
             date={goal.date}
+            time={goal.time}
             description={goal.description}
           />
         ))}
       </div>
-      <button onClick={handleCreateGoal}>Create Goal</button>
-      <ToastContainer autoClose={5000} />
     </div>
   );
 }
