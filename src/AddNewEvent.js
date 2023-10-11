496c0886a2040735e09b5ff786efe5b16ec94173
adamdross27 <104739164+adamdross27@users.noreply.github.com>
2023-10-11 22:49:10 +0000

    Added toastify and database callings

--- src/AddNewEvent.js
+++ src/AddNewEvent.js
@@ -1,5 +1,12 @@
 import React, {useState} from "react";
 import Modal from 'react-modal';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { auth, ref, set, db } from './firebase';
+import eventTypes from "./eventTypes";
+
+const user = auth.currentUser;
+const userID = user ? user.uid : '';
 
 const AddNewEvent = ({ isNewEventTypeModalOpen, setIsNewEventTypeModalOpen, addNewEventType }) => 
 {
@@ -9,21 +16,45 @@ const AddNewEvent = ({ isNewEventTypeModalOpen, setIsNewEventTypeModalOpen, addN
         {
             if(customEventName.trim() !== '')  //Makes sure user enters a string
             {
-                const confirmation = window.confirm(`Your new event type will be called: ${customEventName}`);
-                if(confirmation) //If user clicks yes to confirm new event type
+                const confirm = window.confirm(`Your event type will be called: ${customEventName}`);
+                // console.log("1");
+                
+                if(confirm) //If user clicks yes to confirm new event type
                 {
-                    // Add event type to the dropdown menu. Make sure new event type is at bottom. 
-                   // toast.success('Added New Event Type!');
+                
 
-                    addNewEventType(customEventName.trim())
+                    // Add event type to the dropdown menu. Make sure new event type is at bottom. 
+                    console.log('Toast message should appear');
+
+                   const eventRef = ref(db, `calendar/${userID}/eventTypes`);
+                   set(eventRef, eventTypes)
+                     .then(() => {
+                       // Event added with success
+                       //setIsEventAdded(true);
+                       addNewEventType(customEventName.trim())
+                       toast.success('Adding Event Successfully!');
+                     })
+                     .catch((error) => {
+                       // Event not added
+                       //setIsEventAdded(false);
+                       toast.error('Failed to Add Event.');
+                       console.error('Error adding event: ', error);
+                     });
+               
+
+
+                   // addNewEventType(customEventName.trim())
                     setCustomEventName('');
                     setIsNewEventTypeModalOpen(false);
-                    console.log('test');
+                    toast.success('Added New Event Type!');
+
+
                 }
             }
             else //If no input is entered and they click confirm
             {
                 window.alert(`Please enter a name for your new event type!`);
+                console.log('error occurred...!')
             }
         };
 
@@ -38,7 +69,9 @@ const AddNewEvent = ({ isNewEventTypeModalOpen, setIsNewEventTypeModalOpen, addN
             onRequestClose={() => setIsNewEventTypeModalOpen(false)}
             contentLabel="New Event Type Modal"
         >
-
+            <div>
+                <ToastContainer autoClose={5000} />
+            </div>
             <label htmlFor="customEventName"> New Event Type: </label> 
             <input
                 type="text"
@@ -49,6 +82,7 @@ const AddNewEvent = ({ isNewEventTypeModalOpen, setIsNewEventTypeModalOpen, addN
                 <button onClick={cancelCreateEventType}>Cancel</button> 
                 <button onClick={confirmCreateEventType}>Confirm</button>
             </Modal>
+           
     );
 };
 
