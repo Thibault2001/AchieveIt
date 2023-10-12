75a6f8b1e2b26f5febbabf176d88b08fce99b41a
Thibault2001 <thibaultsera@gmail.com>
2023-10-13 02:53:36 +1300

    try calendar

--- src/dataBaseTest.jsx
+++ src/dataBaseTest.jsx
@@ -1,40 +1,94 @@
-import React, { useState, useEffect,useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import { db, ref, onValue } from './firebase'; // Assurez-vous que le chemin vers votre fichier firebase.js est correct
+import { auth, db, ref, onValue } from './firebase';
+import moment from 'moment';
+import { GetColour } from './Event';
+import databaseTest from './CSS_files/databaseTest.css';
 
 function MyCalendar() {
+  const [currentDate, setCurrentDate] = useState(new Date());
   const [events, setEvents] = useState([]);
+  const [popupPosition, setPopupPosition] = useState({ top: 0, left: 0 });
+  const [selectedEvent, setSelectedEvent] = useState(null); // État pour stocker l'événement sélectionné
   const calendarRef = useRef(null);
+  const [, setUserID] = useState(null);
 
   useEffect(() => {
-    // Référence à la table (nœud) des événements dans la base de données Firebase (adaptez le chemin)
-    const eventsRef = ref(db, 'events');
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        const userID = user.uid;
+        setUserID(userID);
 
-    // Écouter les changements en temps réel dans les événements
-    onValue(eventsRef, (snapshot) => {
-      const eventData = snapshot.val();
-      if (eventData) {
-        const eventArray = Object.entries(eventData).map(([key, value]) => ({
-          id: key,
-          title: value.title,
-          start: value.start,
-          end: value.end,
-        }));
-        setEvents(eventArray);
+        const eventsRef = ref(db, `calendar/${userID}/events`);
+        onValue(eventsRef, (snapshot) => {
+          const eventData = snapshot.val();
+          if (eventData) {
+            const eventArray = Object.entries(eventData).map(([eventID, eventData]) => ({
+              id: eventID,
+              title: eventData.eventTitle,
+              start: moment(eventData.eventDate + 'T' + eventData.eventTime).format('YYYY-MM-DDTHH:mm:ss'),
+              backgroundColor: GetColour(eventData.eventType),
+              borderColor: GetColour(eventData.eventType),
+              description: eventData.eventDescription, // Ajoutez la description de l'événement
+            }));
+            setEvents(eventArray);
+          }
+        });
       }
     });
+
+    return () => unsubscribe();
   }, []);
+
+  // Fonction pour afficher les informations de l'événement sélectionné en popup
+  function displayEventInfo(eventData, position) {
+    setSelectedEvent(eventData);
+    setPopupPosition(position);
+  }
+
+  // Fonction pour fermer le popup d'informations de l'événement
+  function closeEventInfo() {
+    setSelectedEvent(null);
+  }
+
+  // Gestion du clic sur un événement
+  function handleEventClick(info) {
+    const eventData = info.event.extendedProps;
+    const elementRect = info.jsEvent.target.getBoundingClientRect();
+    const position = {
+      top: elementRect.bottom + window.scrollY + 10, // Ajustez la position pour l'affichage du popup
+      left: elementRect.left + window.scrollX,
+    };
+    displayEventInfo(eventData, position);
+  }
 
   return (
     <div>
+      <input
+        type="date"
+        value={currentDate ? moment(currentDate).format('YYYY-MM-DD') : ''}
+        onChange={(e) => setCurrentDate(e.target.value ? new Date(e.target.value) : null)}
+      />
+
       <FullCalendar
         ref={calendarRef}
         plugins={[dayGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
-        events={events} // Utilisation des données d'événements depuis Firebase
+        events={events}
+        eventClick={handleEventClick}
       />
+
+      {/* Affichage des informations de l'événement sélectionné en popup */}
+      {selectedEvent && (
+        <div className="event-popup" style={{ top: popupPosition.top, left: popupPosition.left }}>
+          <h2>{selectedEvent.title}</h2>
+          <p>Date: {selectedEvent.start}</p>
+          <p>Description: {selectedEvent.description}</p> {/* Affichez la description de l'événement */}
+          <button onClick={closeEventInfo}>Fermer</button>
+        </div>
+      )}
     </div>
   );
 }
--- src/welcomeUsers.jsx
+++ src/welcomeUsers.jsx
@@ -4,13 +4,12 @@ import Appointment from "./Appointment";
 import GoalDisplay2 from "./GoalDisplay2";
 import Goal from './Goals';
 import EventDisplay2 from "./eventDisplay2";
-import CalendarDisplay from "./calendarDisplay";
+import CalendarDisplay from "./dataBaseTest";
 import { auth } from './firebase'; // Import Firebase module for authentication
 import { getDatabase, ref, onValue } from 'firebase/database'; // Import Firebase modules for the database
 import { CSSTransition } from 'react-transition-group';
 import './CSS_files/userWelcome.css';
 import AddNewEvent from "./AddNewEvent";
-import eventTypes from "./eventTypes";
 
 const WelcomeUser = () => {
   const [currentView, setCurrentView] = useState("calendar");
