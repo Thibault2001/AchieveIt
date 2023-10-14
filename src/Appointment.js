17671556f7ede30f07728143b86d5d2710c75b7a
BenRogers00 <benr004@outlook.com>
2023-10-14 15:46:03 +1300

    Custom colours working for individual events

--- src/Appointment.js
+++ src/Appointment.js
@@ -50,7 +50,8 @@ import EventDisplay from './EventDisplay' //Importing EventDisplay file
             {id: 1, name: 'Appointment'},
             {id: 2, name: 'Sports'},
             {id: 3, name: 'Birthday'},
-            {id: 4, name: 'University'}
+            {id: 4, name: 'University'},
+            {id: 5, name: 'custom test'}
         ]
 
     return (
