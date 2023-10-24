b79e157de2ea83382e4de8830831919b6599a53f
adamdross27 <104739164+adamdross27@users.noreply.github.com>
2023-10-24 06:25:56 +0000

    minor change to fix toastify notifications on main screen

--- src/welcomeUsers.jsx
+++ src/welcomeUsers.jsx
@@ -12,6 +12,7 @@ import './CSS_files/userWelcome.css';
 import './CSS_files/databaseTest.css';
 import AddNewEvent from "./AddNewEvent";
 import Reminders from "./Reminder.js";
+import { ToastContainer } from 'react-toastify';
 
 const WelcomeUser = () => {
   const [currentView, setCurrentView] = useState("calendar");
@@ -110,6 +111,7 @@ const WelcomeUser = () => {
   return (
     <CSSTransition in={true} appear={true} timeout={500} classNames="page">
       <Box className="main-container" p={4}>
+        <ToastContainer/>
         <Typography variant="h4" fontSize={50} gutterBottom>
           Welcome {userName ? userName : localStorage.getItem('userName')}! Here is your day
           <div className="addEventButton">
